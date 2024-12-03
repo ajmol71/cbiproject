31080ae72bc763b297599233664b6695800cfac9
ajmol71 <136875239+ajmol71@users.noreply.github.com>
2024-12-02 22:39:22 -0800

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,30 @@
+
+provider "google" {
+  project = "{{cbiproject2}}"
+  region  = "us-central1"
+  zone    = "us-central1-f"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "cbiproject2:us-central1:mypostgres"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
+
